1d0150f2ac7fc1150c7c6e502d2fa69cf6fad7b7
Jonathan Schroeder <jon.j.schroeder@gmail.com>
2020-08-18 21:33:14 -0700

    first commit

--- myApp.js
+++ myApp.js
@@ -26,7 +26,7 @@ var app = express();              // Do Not Edit
 // if they see that your site is powered by Express. `X-Powered-By: Express`
 // is sent in every request coming from Express by default.
 
-// The `hidePoweredBy` middleware will remove the `X-Powered-By` header.
+// The `hidePowere dBy` middleware will remove the `X-Powered-By` header.
 // You can also explicitly set the header to something else, to throw
 // people off. e.g. `helmet.hidePoweredBy({ setTo: 'PHP 4.2.0' })`
 
